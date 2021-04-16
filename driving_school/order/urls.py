52542b5eb2a56fc1fd56a04676df13ab900731ad
jtwy23 <jtwy_23@hotmail.com>
2021-04-16 08:23:26 +0000

    Created customer order page

---
+++ driving_school/order/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('my_orders', views.my_orders, name='my_orders'),
+    
+]
--- driving_school/order/views.py
+++ driving_school/order/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def my_orders(request):
+    return render(request, 'my_order.html')
