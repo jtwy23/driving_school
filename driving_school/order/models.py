bf71a7e11b468098c01fde0fac2053638ce85a44
jtwy23 <jtwy_23@hotmail.com>
2021-04-16 10:00:05 +0000

    Added new table for customer order details

--- driving_school/order/models.py
+++ driving_school/order/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from datetime import datetime
+from instructor.models import instructor_information
+from home.models import products
 # Create your models here.
+
+
+# order table for store all order details
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Lesson = models.ForeignKey(products, on_delete=models.CASCADE, blank=True, null=True)
+    Instructor = models.ForeignKey(instructor_information, on_delete=models.CASCADE, blank=True, null=True)
+    order_id = models.CharField(max_length=1000)
+    Lesson_price = models.CharField(max_length=1000)
+    first_name = models.CharField(max_length=1000)
+    last_name = models.CharField(max_length=1000)
+    email = models.CharField(max_length=1000)
+    phone = models.CharField(max_length=100)
+    address = models.CharField(max_length=1000)
+    zip = models.CharField(max_length=1000)
+    ordered = models.BooleanField(default=False)
+    instructor_cancel_order = models.BooleanField(default=False)
+    customer_cancel_order = models.BooleanField(default=False)
+    # order_date = models.DateField(default=datetime.now(), blank=True)
+    order_date = models.CharField(max_length=1000)
+
