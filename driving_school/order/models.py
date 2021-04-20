f5ec1deb692c87609b68b48b3a33bad136efb4e1
jtwy23 <jtwy_23@hotmail.com>
2021-04-20 00:48:18 +0000

    Making python code pep8 compliant

--- driving_school/order/models.py
+++ driving_school/order/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime
 from instructor.models import instructor_information
 from home.models import products
 # Create your models here.
