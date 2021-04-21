bc790ce01b85704da01d2b50f8b0f938405f5cf9
jtwy23 <jtwy_23@hotmail.com>
2021-04-21 04:51:37 +0000

    Changed code in AWS if statement

--- driving_school/driving_school/settings.py
+++ driving_school/driving_school/settings.py
@@ -151,7 +151,7 @@ STATICFILES_DIRS = [
 
 # for image, video upload
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/' 'eu-west-2'
 
 
 if 'USE_AWS' in os.environ:
@@ -160,7 +160,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
