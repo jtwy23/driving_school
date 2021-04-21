b860eb4fa64efda35b68af1d6a4b6b6b2d74eb17
jtwy23 <jtwy_23@hotmail.com>
2021-04-21 04:57:35 +0000

    Added cache control

--- driving_school/driving_school/settings.py
+++ driving_school/driving_school/settings.py
@@ -155,6 +155,12 @@ MEDIA_URL = '/media/' 'eu-west-2'
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'driving-school-v1'
     AWS_S3_REGION_NAME = 'eu-west-2'
