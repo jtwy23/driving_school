6b15a3d8aa79453fb059232bb6290058bdc76e6e
jtwy23 <jtwy_23@hotmail.com>
2021-04-21 04:00:37 +0000

    Added code that automatically collects static files in s3 bucket

--- driving_school/driving_school/settings.py
+++ driving_school/driving_school/settings.py
@@ -48,8 +48,9 @@ INSTALLED_APPS = [
     'our_company',
     'contact',
 
-    # crispy_forms for make form more beautiful
+    # other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -151,6 +152,25 @@ STATICFILES_DIRS = [
 # for image, video upload
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+if 'USE_AWS' in os.environ:
+    # Bucket config
+    AWS_STORAGE_BUCKET_NAME = 'driving-school-v1'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
 # Default primary key field type
