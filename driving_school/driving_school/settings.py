aabd3ef27f56aacc842577a1b4b03ac0c1caa411
jtwy23 <jtwy_23@hotmail.com>
2021-04-21 12:19:06 +0000

    changed location of custom storages

--- driving_school/driving_school/settings.py
+++ driving_school/driving_school/settings.py
@@ -142,16 +142,12 @@ USE_TZ = True
 
 
 # for static files loadins
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-
-# for image, video upload
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/' 'eu-west-2'
 
 
 if 'USE_AWS' in os.environ:
@@ -177,7 +173,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in productions
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
